5de109dee53aab0e9f0a851434e797ba2f51ed8b
Angel Rivera <ariv3ra@gmail.com>
2021-01-22 13:57:21 -0500

    pinned the kubernetes provider to var 1.13.3

--- part03/iac_kubernetes_app/main.tf
+++ part03/iac_kubernetes_app/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "~>0.12"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.13.3"
+    }
+  }  
   backend "remote" {
     organization = "datapunks"
     workspaces {
